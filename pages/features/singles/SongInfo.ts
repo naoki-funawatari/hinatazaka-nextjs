f18332f99fd5caa03b203047fbd29678bce5ad0f
naoki-funawatari <g011c1180@gmail.com>
2021-10-20 14:21:37 +0900

    feat: データを読み込んで表示。

--- pages/features/singles/SongInfo.ts
+++ pages/features/singles/SongInfo.ts
@@ -1,5 +1,5 @@
 type SongInfo = {
-  id: number;
+  id: string;
   title: string;
   songOrder: number;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
+import singles from "./data/singles.json";
+import Header from "./features/Header";
+import Navigation from "./features/Navigation";
+import Main from "./features/Main";
 
 const Home: NextPage = () => {
-  return <h1>NEXTのテンプレート</h1>;
+  const titlePrefix = singles[0].titlePrefix;
+  const title = singles[0].songs[0].title;
+  const id = singles[0].songs[0].id;
+
+  return (
+    <div className="app">
+      <Header />
+      <Navigation singles={singles} />
+      <Main titlePrefix={titlePrefix} title={title} id={id} />
+    </div>
+  );
 };
 
 export default Home;
