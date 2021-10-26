59d269dce21f06f0b5ec4fdbd8b031740144009a
naoki-funawatari <g011c1180@gmail.com>
2021-10-26 10:00:28 +0900

    feat: ベースのルーティングを作成。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
-import { useState, useCallback, useMemo } from "react";
+import Link from "next/link";
+// import { useState, useCallback, useMemo } from "react";
 import singles from "../data/singles.json";
-import Header from "../components/Header";
-import Navigation from "../components/Navigation";
-import Main from "../components/Main";
+// import Header from "../components/Header";
+// import Navigation from "../components/Navigation";
+// import Main from "../components/Main";
 
 export type DisplayParameter = {
   titlePrefix: string;
@@ -12,30 +13,50 @@ export type DisplayParameter = {
 };
 
 const Home: NextPage = () => {
-  const single = singles[0];
-  const song = single.songs[0];
-  const defaultParameter: DisplayParameter = useMemo(
-    () => ({
-      titlePrefix: single.titlePrefix,
-      title: song.title,
-      id: song.id,
-    }),
-    [single, song]
-  );
-  const [displayParameter, setDisplayParameter] =
-    useState<DisplayParameter>(defaultParameter);
-  const handleTitleClicked = useCallback(
-    (): void => setDisplayParameter(defaultParameter),
-    [setDisplayParameter, defaultParameter]
+  return (
+    <ul>
+      {singles.map((single) => (
+        <li key={single.singleOrder}>
+          <Link href={`${single.titlePrefix}`}>{single.titlePrefix}</Link>
+          <ul>
+            {single.songs.map((song) => (
+              <li key={`${single.singleOrder}_${song.songOrder}`}>
+                <Link href={`${single.titlePrefix}/${song.title}`}>
+                  {song.title}
+                </Link>
+                <ul></ul>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
   );
 
-  return (
-    <div className="app">
-      <Header {...{ handleTitleClicked }} />
-      <Navigation {...{ singles, setDisplayParameter }} />
-      <Main {...displayParameter} />
-    </div>
-  );
+  // const single = singles[0];
+  // const song = single.songs[0];
+  // const defaultParameter: DisplayParameter = useMemo(
+  //   () => ({
+  //     titlePrefix: single.titlePrefix,
+  //     title: song.title,
+  //     id: song.id,
+  //   }),
+  //   [single, song]
+  // );
+  // const [displayParameter, setDisplayParameter] =
+  //   useState<DisplayParameter>(defaultParameter);
+  // const handleTitleClicked = useCallback(
+  //   (): void => setDisplayParameter(defaultParameter),
+  //   [setDisplayParameter, defaultParameter]
+  // );
+
+  // return (
+  //   <div className="app">
+  //     <Header {...{ handleTitleClicked }} />
+  //     <Navigation {...{ singles, setDisplayParameter }} />
+  //     <Main {...displayParameter} />
+  //   </div>
+  // );
 };
 
 export default Home;
