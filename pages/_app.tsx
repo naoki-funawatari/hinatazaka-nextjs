63957b78b9357dffc96fafa00b345065cebaa2a1
naoki-funawatari <g011c1180@gmail.com>
2021-11-08 16:11:26 +0900

    feat: スタイルの移動。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
-import "../styles/globals.scss";
-import "../styles/bootstrap/bootstrap.scss";
+import "../src/styles/globals.scss";
+import "../src/styles/bootstrap/bootstrap.scss";
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
