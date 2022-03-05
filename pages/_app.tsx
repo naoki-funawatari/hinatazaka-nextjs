5f9e43a5e42b3def09cf00eb692b1d7ac7d8296f
naoki-funawatari <g011c1180@gmail.com>
2022-03-05 21:25:03 +0900

    feat: 不要なファイルを整理。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,12 @@
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useEffect } from "react";
 import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import "@/styles/globals.scss";
 import "@/styles/bootstrap/bootstrap.scss";
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
-  useEffect(() => {
-    const loadBootstrap = async () => {
-      await import("@/plugin/bootstrap/index.esm");
-    };
-
-    if (process.browser) {
-      loadBootstrap();
-    }
-  }, []);
-
   return (
     <div className="app">
       <Head>
